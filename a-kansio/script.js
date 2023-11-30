57281c99a373bcc60e6dc2aa1188189168a229e8
apeura <anni.peura@tuni.fi>
2023-11-30 23:36:35 +0200

    script.js: Remove comments.

--- a-kansio/script.js
+++ a-kansio/script.js
@@ -1,14 +1,11 @@
-// Array of words to cycle through
 const words = ["Full Stack", "Backend", "UX"];
 
-// Function to change the title at intervals
 function changeHero() {
   let index = 0;
   setInterval(() => {
     document.getElementById("hero-text").innerText = words[index];
     index = (index + 1) % words.length;
-  }, 1500); // Change every 1.5 seconds (3000 milliseconds)
+  }, 1500); 
 }
 
-// Call the function when the page loads
 window.onload = changeHero;
